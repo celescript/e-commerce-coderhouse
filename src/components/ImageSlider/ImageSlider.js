ae32374216b71931e002f8681ecab476aee1f4be
celestef00 <celestefernandez00@gmail.com>
2022-01-04 20:09:49 -0300

    added carousel link

--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,10 +1,15 @@
 import React from 'react'
+
+import './ImageSlider.css'
+
 import img3 from '../../assets/itemhero.png'
 import img2 from '../../assets/shippinghero.png'
 import img1 from '../../assets/leatherbiker.png'
-import './ImageSlider.css'
+
 import {Carousel} from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
+import { Link } from 'react-router-dom'
 
 
 function ImageSlider() {
@@ -14,15 +19,21 @@ function ImageSlider() {
 
     return (
       <Carousel className='main-slide'>
-        <div className='slide'>
-          <img className='slide-image' src={img1} alt='#'></img>
-        </div>
-        <div className='slide'>
-          <img  className='slide-image' src={img2} alt='#'></img>
-        </div>
-        <div  className='slide'>
-          <img  className='slide-image'  src={img3} alt='#'></img>
-        </div>
+        <Link to='/product/16'>
+          <div className='slide'>
+            <img className='slide-image' src={img1} alt='#'></img>
+          </div>
+          </Link>
+        <Link to='/category/all'>
+          <div className='slide'>
+            <img  className='slide-image' src={img2} alt='#'></img>
+          </div>
+          </Link>
+        <Link to='/product/14'>
+          <div  className='slide'>
+            <img  className='slide-image'  src={img3} alt='#'></img>
+          </div>
+          </Link>
       </Carousel>
 
     
