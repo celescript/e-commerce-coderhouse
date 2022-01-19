b99ecfeebb31189b548a2ac631845f47697ff3a0
celestef00 <celestefernandez00@gmail.com>
2022-01-19 20:09:10 -0300

    responsive navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,12 +2,13 @@
 import './NavBar.css'
 
 import AppBar from "@mui/material/AppBar";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
 
-import {Link, NavLink} from 'react-router-dom'
+
+import {Link} from 'react-router-dom'
 import CartWidget from '../CartWidget/CartWidget';
 
+import DesktopNav from './DesktopNav';
+import MobileNav from './MobileNav';
 
 
 
@@ -16,39 +17,16 @@ export default function NavBar() {
   return (
     <AppBar position="static">
       <nav>
+      <MobileNav />
+
       <Link className='link' to='/'>
           <div className="container-logo">
              <h1>odele.</h1>
           </div>
         </Link>
         
-        <ul>
-          <ButtonGroup aria-label="text button group">
-            <li>
-              <NavLink className='link' to='/'
-              end={true}>
-                <Button variant='contained' color='secondary'>Home</Button>
-              </NavLink>
-            </li>
-
-            <li>
-               <NavLink className='link' to="/category/all">
-                <Button variant='contained' color='secondary'>Products</Button>
-              </NavLink>
-            </li>
-
-            <li>
-              <NavLink className='link' to="/stores">
-                <Button variant='contained' color='secondary'>Stores</Button>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink className='link' to="/about">
-                <Button  variant='contained' color='secondary'>About</Button>
-              </NavLink>
-            </li>
-          </ButtonGroup>
-        </ul>
+        
+        <DesktopNav />
         
         
         <CartWidget/>
